d820ccfc320dfa71ed19d97f128ab2b2f35ea926
Christopher Robison <cdr@netoasis.net>
2025-03-10 12:08:36 -0700

    Initial checkin of ambassador event management tool

---
+++ people-request.sh
@@ -0,0 +1,2 @@
+curl "https://api.airtable.com/v0/appwHxV0TBit0ka3F/People?view=Grid%20view" \
+  -H "Authorization: Bearer patMjZbsaWTx0F7Xi.fcb10546df8314407ed9dab43e4360246fdb4ac69c9c39648b995589dda10c7d"
